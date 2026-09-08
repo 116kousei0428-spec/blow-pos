221f599dcfeccec5ae3f847194757aa60dfef230
116kousei0428-spec <116kousei0428@gmail.com>
2026-09-08 22:57:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'blow-amami-pos-mobile-v5-anejo';
+const CACHE_NAME = 'blow-amami-pos-mobile-v6-anejo-extension-analytics';
 const STATIC_ASSETS = [
   './index.html',
   './manifest.json',
