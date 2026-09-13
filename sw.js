f8f8d0f8f6d3ddc227a862bedc943c78f5d9363c
116kousei0428-spec <116kousei0428@gmail.com>
2026-09-14 04:41:20 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'blow-amami-pos-mobile-v13-closing-attendance-integrated-sales61';
+const CACHE_NAME = 'blow-amami-pos-mobile-v14-payroll-attendance-consistency62';
 const STATIC_ASSETS = [
   './index.html',
   './manifest.json',
